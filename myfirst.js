4da880abf3cb49ec68198340f4109adcb94a9146
thuyho2016 <thuyho2016@gmail.com>
2020-06-13 16:38:25 -0700

    clean unused files

--- myfirst.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-    res.write("The date and time are currrently: " + dt.myDateTime());
-    res.end();
-}).listen(8080);
